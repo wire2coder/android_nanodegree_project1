56c1c0eec59ac2c1aeb6ec786482a4bfa9fa1765
Andy Murph <murph@email.com>
2018-04-25 08:38:38 -0400

    look at TODO

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -4,8 +4,12 @@ import android.util.Log;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
@@ -20,13 +24,44 @@ public class JsonUtils {
 //        Log.i(">>> parseSandwichJson", json );
 
         try {
+
+
             JSONObject sandwichData = new JSONObject(json);
+//            Log.i(">>> ::: ", sandwichData.toString() );
+
+            JSONObject nameInJson = sandwichData.getJSONObject("name");
+
+            String name = nameInJson.getString("mainName");
+//            Log.i(">>> ___ ", name);
+
+            ArrayList<String> ls1 = new ArrayList<>();
+            ls1.add("a");
+            ls1.add("b");
+            ls1.add("c");
+
+//        Log.i(">>> ___ ", ls1.toString() );
+
+            // TODO: HERE 4/25 make a Sandwich objects
+            Sandwich sandWich = new Sandwich(name, ls1, "origin", "des", "imag", ls1);
+            Log.i(">>> ___ ", sandWich.getMainName() );
+
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-//        Log.i(">>> parseSandwichJson", sandwichData );
-        // TODO: HERE 4/24
+        /*
+        public Sandwich(
+                String mainName,
+                List<String> alsoKnownAs,
+                String placeOfOrigin,
+                String description,
+                String image,
+                List<String> ingredients)
+        */
+
+
+
 
         return null;
     }
